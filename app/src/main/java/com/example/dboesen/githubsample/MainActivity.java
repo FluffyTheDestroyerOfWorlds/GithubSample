8082ff8d0ceadb2bb024cea6c79de9505e2347dd
Dave Boesen <dboesen@mchenry.edu>
2018-10-09 10:39:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dboesen/githubsample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dboesen.githubsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TODO I need to do this
+    }
+}
